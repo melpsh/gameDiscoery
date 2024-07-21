cdf0263153e73e72192d4813dbc9bc8136000429
Melina Pashna <melinaapashna@gmail.com>
2024-07-21 11:11:08 -0700

    skeleton didn't work properly

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,15 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width='300px'>
+        <Skeleton height='200px' />
+        <CardBody>
+            <SkeletonText />
+        </CardBody>
+    </Card>
+  )
+};
+
+export default GameCardSkeleton;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,21 +25,26 @@ interface IFetchGamesResponse {
 const useGames = () =>{
     const [games, setGames] = useState<IGame[]>([]);
     const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(()=>{
         const controller = new AbortController();
-
+        setIsLoading(true);
         apiClient
         .get<IFetchGamesResponse>('/games', {signal: controller.signal})
-        .then((res)=>setGames(res.data.results))
+        .then((res)=>{
+            setGames(res.data.results);
+            setIsLoading(false);
+        })
         .catch((err)=> {
             if(err instanceof CanceledError) return;
             setError(err.message)})
+            setIsLoading(false);
 
         return ()=> controller.abort();
     },[]);
 
-    return {games, error};
+    return {games, error, isLoading};
 }
 
 export default useGames;
